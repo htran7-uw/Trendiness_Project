e3b009371a1b0c401554da029a4594694cebd2c8
Chelsi Gondalia <cgondalia@wisc.edu>
2021-10-18 16:26:18 -0500

    version2

--- word_count.py
+++ word_count.py
@@ -4,15 +4,19 @@ import os
 import json
 import pandas as pd
 import datetime
-import argparse as ap 
+import argparse  
 
-# open Tweets.txt file 
-with open('Tweets.txt', 'r') as f:
-    lines = f.readlines()
 
-df = pd.DataFrame(columns = ['all', 'date', 'text'])
-df['all'] = lines
-df['date'], df['text'] = df['all'].str.split(',',1).str
+parser = argparse.ArgumentParser()
+parser.add_argument('filename')
+args = parser.parse_args()
+with open(args.filename) as file:
+	lines=file.readlines()
+
+df = pd.DataFrame(lines)
+df['date'], df['text'] = df.iloc[0].str.split(',',1).str
+print(df['date'])
+print(df['text'])
 
 def main(phrase):
     count = 0
@@ -22,4 +26,3 @@ def main(phrase):
            if phrase == word:
                count = count + 1
     print('This phrase occurs ' + str(count) + ' times.')
-
