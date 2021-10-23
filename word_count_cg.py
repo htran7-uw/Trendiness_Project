1ac444303e36ffd869050bc53f9c14a8f6bc568d
Hao Tran <htran7@wisc.edu>
2021-10-23 15:10:24 -0500

    Stripped empty spaces that were still left over. Also added in some comments to clarify code.

--- word_count_cg.py
+++ word_count_cg.py
@@ -7,31 +7,29 @@ final=[]
 with open('Tweets.txt') as file:
 	lines = file.readlines()
 
-print(type(lines))
-
 tweets = pd.DataFrame(lines)
-print(tweets.shape)
-print(tweets[0][1])
+#print(tweets.shape)
+#print(tweets[0][1])
 tweets_empty = []
 for i, words in enumerate(tweets[0]):
 	if words == '':
 		tweets_empty.append(i)
+'''Check for empty lists'''
 try:
 	tweets.drop([tweets_empty],axis = 0, inplace=True)
 except KeyError:
 	pass
 tweets.dropna(inplace = True)
+
+'''Break down the tweet and parse words only'''
 for i, words in enumerate(tweets[0]):
 	words = words.strip().lower()
-	print(words)
-	# word_list = re.split(r"[\b\W\b]+", words.strip().lower())
-	# #word_list = word_list.drop()
-	# print(word_list)
-	#word_list = re.split(r'.', words.strip())
 	new_words = re.sub(r'[\d{4}\-\d{2}\-\d{2}\-\d{2}\-\d{2}\-\d{2},]','',words)
 	new_words = re.sub(r'https://t.co/\w+', '', new_words)
 	new_words = re.sub(r'@[a-z0-9\_]+','',new_words)
 	final_list = re.findall(r"[^rt](?!'.*')\b[\w']+\b", new_words)
+	for i, word in enumerate(final_list):
+		final_list[i] = str(word.strip())
 	final.extend(final_list)
 	
 
@@ -40,4 +38,5 @@ values=values.tolist()
 keys=keys.tolist()
 
 res = {keys[i]: values[i] for i in range(len(keys))}
-print(res)
+for count in res.items():
+	print(count)
