f51698bacdf217033f762885d8946c8008a34291
Hao Tran <htran7@wisc.edu>
2021-10-30 19:12:31 -0500

    Made changes to my word_count python file. Also added in the argsparse argument for the server python file. Still stuck on error handling

--- server.py
+++ server.py
@@ -3,22 +3,33 @@ import os
 import json
 import pandas as pd
 import datetime
+import argparse
+import time
+
 #API Key = xWbz9qoquYQ2DdIXwJ6yNJq1G
 #API Key Secret = 1SiQLpVlvTsnlKwHBqfnejuHLBGZUUUdoCFp3GpVytOMmYeLWU
 #Bearer Token = AAAAAAAAAAAAAAAAAAAAAFBhUgEAAAAAhchmgzNuAQeIPOXeYqYx138AOu0%3De7gL0uj0UaOjETomEJJemvl2kdH7S8TNk6jz8SJPsN2rlpfTMt
 
+'''Credentials'''
 os.environ['BEARER_TOKEN'] = 'AAAAAAAAAAAAAAAAAAAAAFBhUgEAAAAAhchmgzNuAQeIPOXeYqYx138AOu0%3De7gL0uj0UaOjETomEJJemvl2kdH7S8TNk6jz8SJPsN2rlpfTMt'
 bearer_token = os.environ.get("BEARER_TOKEN")
-print(bearer_token)
+#print(bearer_token)
 
+'''Optional --filename flag for json files'''
+parser = argparse.ArgumentParser()
+parser.add_argument('json',type=str, nargs = '?', help = 'Load a JSON file instead of the Twitter API')
+args = parser.parse_args()
+json_file = args.json
 
-'''Part A: Authenticate myself as a user and read all tweets'''
-'''Source code: https://github.com/twitterdev/Twitter-API-v2-sample-code/blob/main/Sampled-Stream/sampled-stream.py'''
+'''Authenticate myself as a user and read all tweets'''
+'''Source code: 
+https://github.com/twitterdev/Twitter-API-v2-sample-code/blob/main/Sampled-Stream/sampled-stream.py
+'''
 path = os.getcwd()
-print(path)
+#print(path)
 if os.path.exists('Tweets.txt') == True:
     os.replace('Tweets.txt', 'Tweets.txt')
-    print('We replaced the file, but it has the same name.')
+    print('We replaced the Tweets.txt file so you do not run out of disk space.')
 else:
     open('Tweets.txt', 'w')
     print('We created a brand new file called Tweets.txt.')
@@ -39,6 +50,20 @@ def bearer_oauth(r):
 def connect_to_endpoint(url):
     response = requests.request("GET", url, auth = bearer_oauth, stream = True)
     print(response.status_code)
+    if response.status_code != 200:
+        for x in range(0, 6):
+            print('Attempt #' + str(x))
+            status = r.status_code
+            if status != 200:
+                print('The error code is still ' + str(status) + '.')
+                seconds = 2
+                print("Let's wait", seconds, "seconds and try again")
+                time.sleep(seconds)
+            else:
+                print('Wait it actually worked!')
+                break
+            print('Looks like we still get an error code of', str(status), "You might have the wrong URL inserted.")
+
     for response_line in response.iter_lines():
         if response_line:
             json_response = json.loads(response_line)
@@ -52,22 +77,37 @@ def connect_to_endpoint(url):
                 txt_file.writelines(each_tweet)
             else:
                 pass
-    if response.status_code != 200:
-        raise Exception(
-            "Request returned an error: {} {}".format(
-                response.status_code, response.text
-            )
-        )
+
+
+def extract_json_file(file):
+    f = open(file,'r')
+    data = json.loads(f.read())
+    json_response = json.loads(data)
+    print(json.dumps(json_response, indent=4, sort_keys=True))
+    if json_response['data']['lang'] == 'en':
+        timestamp = json_response['data']['created_at']
+        timestamp = datetime.datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S.%f%z')
+        timestamp = datetime.datetime.strftime(timestamp, '%Y-%m-%d-%H-%M-%S')
+        text = json_response['data']['text']
+        each_tweet = [timestamp, ',', text, '\n']
+        txt_file.writelines(each_tweet)
+    else:
+        pass
 def main():
-    url = create_url()
-    timeout = 0
-    while True:
-        try:
-            connect_to_endpoint(url)
-            timeout += 1
-        except KeyboardInterrupt as err:
-            print(f"Somebody paused the script!")
-            break
+    if json_file:
+        print("Looks like we're going to be working with a JSON file")
+        extract_json_file(json_file)
+    else:
+        url = create_url()
+        timeout = 0
+        while True:
+            try:
+                connect_to_endpoint(url)
+                timeout += 1
+            except KeyboardInterrupt:
+                print(f"Somebody paused the script!",'\n',"Re-run the script to continue. However, you will be overwriting your file if you do." ,'\n'
+                      , "If you do not want to lose that file, store it somewhere else and then re-run the script.")
+                break
 
 if __name__ == "__main__":
     main()
--- word_count_ht.py
+++ word_count_ht.py
@@ -1,48 +1,85 @@
+#!FULL_PATH_TO_PYTHON_INTERPRTER -m spacy download en
 import re
 import pandas as pd
 import numpy as np
+import argparse
+import spacy
 
-with open('Tweets.txt') as file:
-	lines = file.readlines()
+parser = argparse.ArgumentParser()
+parser.add_argument('filename', type=str)
+args = parser.parse_args()
 
-#print(type(lines))
+final = []
 
-tweets = pd.DataFrame(lines)
-#print(tweets.shape)
-#print(tweets[0][1])
-tweets_empty = []
-for i, words in enumerate(tweets[0]):
-	if words == '':
-		tweets_empty.append(i)
-try:
-	tweets.drop([tweets_empty],axis = 0, inplace=True)
-except KeyError:
-	pass
-tweets.dropna(inplace = True)
+filename = args.filename
+nlp = spacy.load('en_core_web_sm', disable=['parser', 'ner'])
+regex = r"""(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|
+        mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br
+        |bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo
+        |jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b/?(?!@)))"""
 
-final_list = []
+def get_file(filename):
+    with open(filename) as file:
+        lines = file.readlines()
+        tweets = pd.DataFrame(lines)
+    return tweets
 
-for i, words in enumerate(tweets[0]):
-	words = words.strip().lower()
-	#print(words)
-	new_words = re.sub(r'[\d{4}\-\d{2}\-\d{2}\-\d{2}\-\d{2}\-\d{2},]','',words)
-	new_words = re.sub(r'https://t.co/\w+', '', new_words)
-	new_words = re.sub(r'@[a-z0-9\_]+','',new_words)
-	#print(new_words)
-	#Source code: https://stackoverflow.com/questions/27715581/how-do-i-handle-contractions-with-regex-word-boundaries-in-javascript
-	word_list = re.findall(r"[^rt](?!'.*')\b[\w']+\b", new_words)
-	final_list.append(word_list)
+def clean_text(text):
+    if type(text) != str:
+        text = text.decode("utf-8")
+    doc = re.sub(regex, '', text, flags=re.MULTILINE)  # remove URLs
+    sentences = []
+    for sentence in doc.split("\n"):
+        if len(sentence) == 0:
+            continue
+        sentences.append(sentence)
+    doc = nlp("\n".join(sentences))
+    doc = " ".join([token.lemma_.lower().strip() for token in doc
+                    if (not token.is_stop)
+                    and (not token.like_url)
+                    and (not token.lemma_ == "-PRON-")
+                    and (not len(token) < 4)])
+    return doc
 
-word_count = {}
-for each_tweet in final_list:
-	for word in each_tweet:
-		word = word.strip()
-		if word in word_count:
-			word_count[word] += 1
-		else:
-			word_count[word] = 1
-#print(word_count)
-for count in word_count.items():
-	print(count)
+def parse_tweets(tweets):
+    tweets_empty = []
+    for i, words in enumerate(tweets[0]):
+        if words == '':
+            tweets_empty.append(i)
+    '''Check for empty lists'''
+    try:
+        tweets.drop([tweets_empty], axis=0, inplace=True)
+    except KeyError:
+        pass
+    tweets.dropna(inplace=True)
+
+    '''Break down the tweet and parse words only'''
+    for i, words in enumerate(tweets[0]):
+        new_words = clean_text(words)
+        new_words = re.sub(r'[\d{4}\-\d{2}\-\d{2}\-\d{2}\-\d{2}\-\d{2},]', '', new_words)
+        new_words = re.sub(r'@[a-z0-9\_]+', '', new_words)
+        new_words = re.sub(r'\brt\b', '', new_words)
+        final_list = re.findall(r"[^rt](?!'.*')\b[\w']+\b", new_words)
+        #print(final_list)
+        for i, word in enumerate(final_list):
+            final_list[i] = str(word.strip())
+        final.extend(final_list)
+
+    '''Compute unique words and their corresponding frequencies'''
+    keys, values = np.unique(final, return_counts=True)
+    values = values.tolist()
+    keys = keys.tolist()
+
+    res = {keys[i]: values[i] for i in range(len(keys))}
+    for item_count in res.items():
+        print(item_count)
+
+def main():
+    twitter_df = get_file(filename)
+    parse_tweets(twitter_df)
+    print('Done!')
+
+if __name__ == '__main__':
+    main()
 
 
