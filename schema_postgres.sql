5e04fefc93da81e63b9a5d050f580c8b85dbe67f
mplantz <90726562+mplantz@users.noreply.github.com>
2021-11-16 17:39:53 -0600

    Update schema_postgres.sql
    added additional logic to ensure the unique ID is the same in both dimensional columns. The issue I am running into now is that I am not sure what the file path for the tweets data lake would be.

--- schema_postgres.sql
+++ schema_postgres.sql
@@ -1,27 +1,52 @@
 -- Basis for schema_postgres.sql
 -- Still incomplete but is the basis for the sql commands for milestone 2
 
-CREATE OR REPLACE TABLE fact (
-	unique_id SERIAL,
-	date TIMESTAMP,
-	time INTEGER,
-	tweet_text TEXT,
-	PRIMARY KEY (unique_id)
+CREATE OR REPLACE TABLE base (
+	UNIQUE_ID INT GENERATED ALWAYS AS IDENTITY 
+	t TIMESTAMP,
+	tex TEXT
 	);
-
-CREATE OR REPLACE TABLE time (
-	unique_id SERIAL,
-	date TIMESTAMP,
-	time TIMESTAMP,
-	PRIMARY KEY (unique_id)
-	);
-
-COPY fact(date, tweet_text)
-FROM ### insert tweets.txt file location here ###
-DELIMITER ‘,’
+	
+COPY base(t, tex)
+FROM '### insert tweets.txt file path here'
+DELIMITER ','
 CSV HEADER;
 
-COPY time(date)
-FROM ### insert tweets.txt file location here ###
-DELIMITER ‘,’
-CSV HEADER;
+-- This section alters the timestamp column into a time and a date column	
+BEGIN;
+
+ALTER TABLE base ADD COLUMN dat date;
+ALTER TABLE base ADD COLUMN tim time;
+
+UPDATE base
+SET
+    dat = t::date,
+    tim = t::time;
+
+ALTER TABLE base DROP COLUMN t;  
+
+COMMIT;
+
+-- creating dimensional tables
+
+CREATE OR REPLACE TABLE txt (
+	UNIQUE_ID SERIAL,
+	DAT TIMESTAMP,
+	TIM TIMESTAMP,
+	TWEET_TXT TEXT,
+	PRIMARY KEY (UNIQUE_ID)
+	)
+CREATE OR REPLACE TABLE time_date(
+	UNIQUE_ID SERIAL,
+	DAT TIMESTAMP,
+	TIM TIMESTAMP,
+	PRIMARY KEY (UNIQUE_ID)
+	)
+
+-- Copy data from base table to dimensional tables
+
+COPY txt(UNIQUE_ID, DAT, TIM, TWEET_TXT)
+FROM base;
+
+COPY time_date(UNIQUE_ID, DAT, TIM)
+FROM base;
