6f06f457e01b0350c5e84cbab8d906d767edeef2
mplantz <90726562+mplantz@users.noreply.github.com>
2021-11-18 12:18:33 -0600

    Update schema_postgres.sql
    Update to the sql code to add more tables to the data warehouse. No need to link between tables as it is just data entry.

--- schema_postgres.sql
+++ schema_postgres.sql
@@ -1,52 +1,21 @@
--- Basis for schema_postgres.sql
--- Still incomplete but is the basis for the sql commands for milestone 2
-
-CREATE OR REPLACE TABLE base (
-	UNIQUE_ID INT GENERATED ALWAYS AS IDENTITY 
+DROP TABLE IF EXISTS base;
+CREATE TABLE base(
+	UNIQUE_ID INTEGER GENERATED ALWAYS AS IDENTITY, 
 	t TIMESTAMP,
-	tex TEXT
+	tex VARCHAR(1000) NOT NULL
 	);
 	
-COPY base(t, tex)
-FROM '### insert tweets.txt file path here'
-DELIMITER ','
-CSV HEADER;
+CREATE TABLE WORDS (
+	TIME TIMESTAMP,
+	WORD VARCHAR(1000)
+	);
+CREATE TABLE PHRASES (
+	TIME TIMESTAMP,
+	PHRASE VARCHAR(1000)
+	);
+CREATE TABLE FINAL (
+	TIME TIMESTAMP,
+	UNIQUE_WORD VARCHAR(1000),
+	FREQUENCY INTEGER
+	);
 
--- This section alters the timestamp column into a time and a date column	
-BEGIN;
-
-ALTER TABLE base ADD COLUMN dat date;
-ALTER TABLE base ADD COLUMN tim time;
-
-UPDATE base
-SET
-    dat = t::date,
-    tim = t::time;
-
-ALTER TABLE base DROP COLUMN t;  
-
-COMMIT;
-
--- creating dimensional tables
-
-CREATE OR REPLACE TABLE txt (
-	UNIQUE_ID SERIAL,
-	DAT TIMESTAMP,
-	TIM TIMESTAMP,
-	TWEET_TXT TEXT,
-	PRIMARY KEY (UNIQUE_ID)
-	)
-CREATE OR REPLACE TABLE time_date(
-	UNIQUE_ID SERIAL,
-	DAT TIMESTAMP,
-	TIM TIMESTAMP,
-	PRIMARY KEY (UNIQUE_ID)
-	)
-
--- Copy data from base table to dimensional tables
-
-COPY txt(UNIQUE_ID, DAT, TIM, TWEET_TXT)
-FROM base;
-
-COPY time_date(UNIQUE_ID, DAT, TIM)
-FROM base;
