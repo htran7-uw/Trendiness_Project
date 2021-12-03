094fcef098b40a0a931835d9d899a3d04529e129
Xinyuan Zhang <90668265+xinyuanlzhang@users.noreply.github.com>
2021-12-03 08:22:07 -0600

    Create keys_for_m3.py

---
+++ keys_for_m3.py
@@ -0,0 +1,5 @@
+consumer_key ='yGHTfRpWohNDY9e9a0qkBuLEj'
+consumer_secret ='mcZIQa4JoV23yaZ2fzVau5Bca4rIWZldgBwrTbEQhNnSdrq3z1'
+access_token ='936944933671280640-My9PfhyLJ9PWW8ujMas85NXmn6ROaQu'
+access_token_secret ='alrUbIwOTj6KC3ZQLnhus3RPu1qJT0XSOSSQeMUpf98iS'
+BEARER_TOKEN = 'AAAAAAAAAAAAAAAAAAAAAJw5VgEAAAAAnZeXIeiz%2FUkzebmqcQeX8uTZH84%3Dkh5CV71bUCK1OOu4I3TAigdCpWkOicAHxg3ygEm6gVSBOPvNXs'
